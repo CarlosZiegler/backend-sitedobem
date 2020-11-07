7b7e44bc0b665c16f9f7dbe32e58112c69f02428
carlosziegler <carlos.ziegler@gmail.com>
2020-11-08 00:50:48 +0100

    fix create vacancy and update user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,36 +1,50 @@
-const User = require('../models/User.js')
+const User = require("../models/User.js");
 
 module.exports = {
-
-    async show(req, res, next) {
-        try {
-            const { role, vacancies, _id, email, professionalProfile, displayName } = await User.findOne({ _id: req.user._id }).populate('professionalProfile').populate('vacancies')
-
-            res.json({ role, vacancies, _id, email, professionalProfile, displayName })
-        } catch (error) {
-            res.status(404).json(error)
-        }
-    },
-    async destroy(req, res, next) {
-        try {
-            const result = await User.deleteOne({ _id: _id })
-            if (result.deletedCount === 1) {
-                return res.status(200).json({ message: "User deleted" })
-            }
-        } catch (error) {
-            return res.status(404).json(error)
-        }
-    },
-    async update(req, res, next) {
-        const _id = req.user._id
-        let data = req.body
-        try {
-            const result = await User.updateOne({ _id: _id }, data)
-            if (result.nModified === 1) {
-                return res.status(200).json({ message: "User updated" })
-            }
-        } catch (error) {
-            return res.status(404).json(error)
-        }
+  async show(req, res, next) {
+    try {
+      const {
+        role,
+        vacancies,
+        _id,
+        email,
+        professionalProfile,
+        displayName,
+      } = await User.findOne({ _id: req.user._id })
+        .populate("professionalProfile")
+        .populate("vacancies");
+      res.json({
+        role,
+        vacancies,
+        _id,
+        email,
+        professionalProfile,
+        displayName,
+      });
+    } catch (error) {
+      res.status(404).json(error);
     }
-}
+  },
+  async destroy(req, res, next) {
+    try {
+      const result = await User.deleteOne({ _id: _id });
+      if (result.deletedCount === 1) {
+        return res.status(200).json({ message: "User deleted" });
+      }
+    } catch (error) {
+      return res.status(404).json(error);
+    }
+  },
+  async update(req, res, next) {
+    const _id = req.user._id;
+    let data = req.body;
+    try {
+      const result = await User.updateOne({ _id: _id }, data);
+      if (result.nModified === 1) {
+        return res.status(200).json({ message: "User updated" });
+      }
+    } catch (error) {
+      return res.status(404).json(error);
+    }
+  },
+};
--- controllers/VacancyController.js
+++ controllers/VacancyController.js
@@ -1,82 +1,75 @@
-const Vacancy = require('../models/Vacancy.js')
-const User = require('../models/User.js')
-
+const Vacancy = require("../models/Vacancy.js");
+const User = require("../models/User.js");
 
 module.exports = {
-    async index(req, res, next) {
-        try {
-            // TODO only find jobs ?
-            const vacancies = await Vacancy.find()
-            res.json(vacancies)
-        } catch (error) {
-            res.json(error)
-        }
-    },
-    async show(req, res, next) {
-        try {
-            const vacancy = await Vacancy.findOne({ _id: req.params.id })
-            res.json(vacancy)
-        } catch (error) {
-            res.status(404).json(error)
-        }
-    },
-    async store(req, res, next) {
-
-        if (req.user.role !== 'company') {
-            return res.json({ error: "not permitted" });
-        }
-
-        const {
-            title,
-            description,
-            tags,
-            location,
-            contact,
-            companyId = req.user._id,
-        } = req.body
-
-        const tagsWithoutSpace = tags.split(',').map(tag => tag.toLowerCase().trim())
-
-        try {
-            const result = await Vacancy.create({
-                title,
-                description,
-                tags: tagsWithoutSpace,
-                location,
-                companyId,
-                contact
-            })
-
-            const userUpdated = await User.updateOne({ _id: companyId },
-                { vacancies: [...req.user.vacancies, result] })
-
-            return res.json(userUpdated);
-        } catch (error) {
-            console.log(error)
-        }
-
-    },
-    async destroy(req, res, next) {
-        const _id = req.params.id
-        try {
-            const result = await Vacancy.deleteOne({ _id: _id })
-            if (result.deletedCount === 1) {
-                return res.status(200).json({ message: "Vacancy deleted" })
-            }
-        } catch (error) {
-            return res.status(404).json(error)
-        }
-    },
-    async update(req, res, next) {
-        const _id = req.params.id
-        let data = req.body
-        try {
-            const result = await Vacancy.updateOne({ _id: _id }, data)
-            if (result.nModified === 1) {
-                return res.status(200).json({ message: "Vacancy updated" })
-            }
-        } catch (error) {
-            return res.status(404).json(error)
-        }
+  async index(req, res, next) {
+    try {
+      // TODO only find jobs ?
+      const vacancies = await Vacancy.find();
+      res.json(vacancies);
+    } catch (error) {
+      res.json(error);
     }
-}
+  },
+  async show(req, res, next) {
+    try {
+      const vacancy = await Vacancy.findOne({ _id: req.params.id });
+      res.json(vacancy);
+    } catch (error) {
+      res.status(404).json(error);
+    }
+  },
+  async store(req, res, next) {
+    if (req.user.role !== "company") {
+      return res.json({ error: "not permitted" });
+    }
+    const { title, description, tags, location, contact } = req.body;
+
+    const tagsWithoutSpace = tags
+      .split(",")
+      .map((tag) => tag.toLowerCase().trim());
+
+    try {
+      const result = await Vacancy.create({
+        title,
+        description,
+        tags: tagsWithoutSpace,
+        location,
+        companyId: req.user._id,
+        contact,
+      });
+
+      await User.updateOne(
+        { _id: req.user._id },
+        { $addToSet: { vacancies: [result._id] } }
+      );
+
+      return res.json(req.user);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async destroy(req, res, next) {
+    const _id = req.params.id;
+    try {
+      const result = await Vacancy.deleteOne({ _id: _id });
+      if (result.deletedCount === 1) {
+        return res.status(200).json({ message: "Vacancy deleted" });
+      }
+    } catch (error) {
+      return res.status(404).json(error);
+    }
+  },
+  async update(req, res, next) {
+    const _id = req.params.id;
+    let data = req.body;
+    try {
+      const result = await Vacancy.updateOne({ _id: _id }, data);
+      if (result.nModified === 1) {
+        return res.status(200).json({ message: "Vacancy updated" });
+      }
+    } catch (error) {
+      return res.status(404).json(error);
+    }
+  },
+};
